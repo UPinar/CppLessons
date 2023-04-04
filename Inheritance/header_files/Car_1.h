f2a40d8e41c0cd9e62604b7d827249ec69245379
UPinar <72672033+UPinar@users.noreply.github.com>
2023-04-04 12:18:54 +0300

    Add files via upload

---
+++ Inheritance/header_files/Car_1.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <iostream>
+
+class Car {
+public:
+	virtual void start()
+	{
+		std::cout << "Car has just started\n";
+	}
+	virtual void run()
+	{
+		std::cout << "Car is running\n";
+	}
+	virtual void stop()
+	{
+		std::cout << "Car has just stopped\n";
+	}
+};
+
+class Mercedes : public Car {
+public:
+	void start()override
+	{
+		std::cout << "Mercedes has just started\n";
+	}
+	void run()override
+	{
+		std::cout << "Mercedes is running\n";
+	}
+	void stop()override
+	{
+		std::cout << "Mercedes has just stopped\n";
+	}
+};
+
+class Volvo : public Car {
+public:
+	void start()override
+	{
+		std::cout << "Volvo has just started\n";
+	}
+	void run()override
+	{
+		std::cout << "Volvo is running\n";
+	}
+	void stop()override
+	{
+		std::cout << "Volvo has just stopped\n";
+	}
+};
+
+class VolvoXC90 : public Volvo {
+public:
+	void start()override
+	{
+		std::cout << "VolvoXC90 has just started\n";
+	}
+	// void run()override
+	// {
+	// 	std::cout << "VolvoXC90 is running\n";
+	// }
+	void stop()override
+	{
+		std::cout << "VolvoXC90 has just stopped\n";
+	}
+};
+
