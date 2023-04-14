b74dbefc0f7bfb117950336908d6b64ee078918b
UPinar <72672033+UPinar@users.noreply.github.com>
2023-04-14 16:44:38 +0300

    Add files via upload

---
+++ FunctionOverloading/c_files/different.c
@@ -0,0 +1,6 @@
+#include "different.h"
+
+int func_difference(int a, int b)
+{
+	return a * a + b * b - 3;
+}
---
+++ FunctionOverloading/c_files/same.c
@@ -0,0 +1,6 @@
+#include "same.h"
+
+int func_same(int a, int b)
+{
+	return a * a + b * b - 1;
+}
